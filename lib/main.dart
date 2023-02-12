21e56ae134a2f31b633344a991cf2774caf5550a
Sam H <samuelhussain@live.com>
2023-02-12 14:34:14 -0500

    removed logging package (prefer using printDebug)

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,13 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:logger/logger.dart';
 import 'package:stride_squad/homepage.dart';
 
+//Use debugPrint() to print stuff
 void main() {
   //Keeps device in portrait rotation
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  var logger = Logger();
-  if (kReleaseMode) {
-    logger.i("Stride Squad is running in release mode");
-  } else if (kProfileMode) {
-    logger.i("Stride Squad is running in profile mode");
-  } else {
-    logger.i("Stride Squad is running in debug mode");
-  }
-  runApp(const MyApp());
+  runApp(const MyApp());  
 }
 
 class MyApp extends StatelessWidget {
