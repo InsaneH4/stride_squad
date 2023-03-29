cd16e68bde1a7df769558ad556695a96fc553180
Sam H <samuelhussain@live.com>
2023-03-29 18:36:05 -0400

    added sizedbox

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -102,33 +102,34 @@ class _LeaderboardState extends State<Leaderboard> {
           itemBuilder: (context, index) => SizedBox(
             height: 90,
             child: Card(
+                color: Colors.white.withOpacity(0.5),
                 child: ListTile(
-              tileColor: Colors.white.withAlpha(0),
-              //shape:Border.all(width: 0.5),
-              trailing: Transform(
-                transform: Matrix4.translationValues(-90, 0.0, 0.0),
-                child: const Text(
-                  'RedDashers',
-                  style: TextStyle(fontSize: 32, color: Colors.black),
-                ),
-              ),
-              leading: Transform(
-                transform: Matrix4.translationValues(2, 0.0, 0.0),
-                child: Text(
-                  '${index + 4}',
-                  style: const TextStyle(fontSize: 26, color: Colors.black),
-                ),
-              ),
-              // onTap: showTeamDialog,
-              title: Transform(
-                transform: Matrix4.translationValues(-50, 0.0, 0.0),
-                child: const CircleAvatar(
-                  radius: 22,
-                  backgroundImage:
-                      AssetImage("image_assets/Stock_LeaderBoard_image.png"),
-                ),
-              ),
-            )),
+                  tileColor: Colors.white.withAlpha(0),
+                  //shape:Border.all(width: 0.5),
+                  trailing: Transform(
+                    transform: Matrix4.translationValues(-90, 0.0, 0.0),
+                    child: const Text(
+                      'RedDashers',
+                      style: TextStyle(fontSize: 32, color: Colors.black),
+                    ),
+                  ),
+                  leading: Transform(
+                    transform: Matrix4.translationValues(2, 0.0, 0.0),
+                    child: Text(
+                      '${index + 4}',
+                      style: const TextStyle(fontSize: 26, color: Colors.black),
+                    ),
+                  ),
+                  // onTap: showTeamDialog,
+                  title: Transform(
+                    transform: Matrix4.translationValues(-50, 0.0, 0.0),
+                    child: const CircleAvatar(
+                      radius: 22,
+                      backgroundImage: AssetImage(
+                          "image_assets/Stock_LeaderBoard_image.png"),
+                    ),
+                  ),
+                )),
           ),
         ),
       ),
