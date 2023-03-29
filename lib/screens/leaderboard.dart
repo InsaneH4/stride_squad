ef08f8a2e6e36113b8af2f161dcac445c87f4cc2
anjunthomas <124937896+anjunthomas@users.noreply.github.com>
2023-03-29 18:00:11 -0400

    Changed List Tile color to white

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -106,7 +106,7 @@ class _LeaderboardState extends State<Leaderboard> {
               scrollDirection: Axis.vertical,
               itemBuilder: (context, index) => Card(
                   child: ListTile(
-                    tileColor: Theme.of(context).primaryColor,
+                    tileColor: Colors.white,
                     //shape:Border.all(width: 0.5),
                     trailing: Transform(
                       transform: Matrix4.translationValues(-160, 0.0, 0.0), 
