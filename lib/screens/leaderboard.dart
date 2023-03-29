5385e83b4b843edc809803c0f0b786d49d4b36ac
anjunthomas <124937896+anjunthomas@users.noreply.github.com>
2023-03-29 18:03:47 -0400

    LeaderBoard Changes: Team # & listBuilder Padding

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -19,9 +19,9 @@ class _LeaderboardState extends State<Leaderboard> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           Container(
-              padding: const EdgeInsets.only(left: 40.0, top: 25.0, right: 40.0, bottom: 4.0),
-              child: Text("Leaderboard",
-                  style: Theme.of(context).textTheme.headlineSmall),
+              padding: const EdgeInsets.only(left: 40.0, top: 30.0, right: 40.0, bottom: 4.0),
+              //child: Text("Leaderboard",
+                //  style: Theme.of(context).textTheme.headlineSmall),
             ),
           // Horizontal Row containing Top 3 Teams
           Row(
@@ -37,17 +37,18 @@ class _LeaderboardState extends State<Leaderboard> {
                         Padding(
                           padding: const EdgeInsets.only(top: 8.0, bottom: 0.1),
                           child: Text(
-                            "Team",
+                            "Team 1",
                             style: Theme.of(context).textTheme.titleSmall
                               )
                           ),
-                        Padding(
+                        /*Padding(
                           padding: const EdgeInsets.only(top: 5.0, bottom: 0.1),
                           child: Text(
                             "1",
                             style: Theme.of(context).textTheme.titleMedium,
                               )
                           ),  
+                          */
                       ]
               ),
               Column(
@@ -60,10 +61,11 @@ class _LeaderboardState extends State<Leaderboard> {
                         Padding(
                           padding: const EdgeInsets.only(top: 8.0, bottom: 0.1),
                           child: Text(
-                            "Team",
+                            "Team 2",
                             style: Theme.of(context).textTheme.titleSmall,
                               )
                           ),
+                          /*
                           Padding(
                           padding: const EdgeInsets.only(top: 5.0, bottom: 0.1),
                           child: Text(
@@ -71,6 +73,7 @@ class _LeaderboardState extends State<Leaderboard> {
                             style: Theme.of(context).textTheme.titleMedium,
                               )
                           ),
+                          */
                       ]
               ),
               Column(
@@ -83,17 +86,18 @@ class _LeaderboardState extends State<Leaderboard> {
                       Padding(
                           padding: const EdgeInsets.only(top: 8.0, bottom: 0.1),
                             child: Text(
-                              "Team",
+                              "Team 3",
                               style: Theme.of(context).textTheme.titleSmall,
                                 )
                             ),
-                            Padding(
+                            /*Padding(
                             padding: const EdgeInsets.only(top: 5.0, bottom: 0.1),
                             child: Text(
                               "3",
                               style: Theme.of(context).textTheme.titleMedium,
                                 )
-                           ),   
+                           ), 
+                           */  
                       ]
               ),
           ],),
@@ -102,6 +106,7 @@ class _LeaderboardState extends State<Leaderboard> {
           ),
           Expanded(
             child: ListView.builder(
+              padding: const EdgeInsets.only(top: 2),
               itemCount: 17,
               scrollDirection: Axis.vertical,
               itemBuilder: (context, index) => Card(
