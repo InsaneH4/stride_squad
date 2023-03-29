3d4c00817b2097272b4f6e022a5e9ef65e7c7078
BenMingst <124937048+BenMingst@users.noreply.github.com>
2023-03-29 19:51:16 -0400

    update color

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
 
-class Chat extends StatefulWidget {
-  const Chat({super.key, required this.title});
+class Chat extends StatelessWidget {
+  const Chat({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<Chat> createState() => _ChatState();
-}
-
-class _ChatState extends State<Chat> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //might add appbar later
-      // appBar: AppBar(
-      //   title: Text(widget.title),
-      // ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              'In progress...',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xff65c7f7),
+              Color(0xff9cecfb),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'In progress...',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
         ),
       ),
     );
