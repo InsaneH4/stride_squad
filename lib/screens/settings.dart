4d9cf4eb8c7723c6984f7b199f037d3d6a2061a8
BenMingst <124937048+BenMingst@users.noreply.github.com>
2023-03-29 19:26:43 -0400

    settings modernization

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -28,41 +28,119 @@ class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        title: Text(
+          'Settings',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+      ),
+      body: Container(
+        color: Theme.of(context).primaryColor,
         child: Column(
-          //      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'In progress...',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            ElevatedButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const Profile(title: "Profile"),
+            SizedBox(height: 24),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                'General',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              child: const Text(
-                "View Profile",
-                style: TextStyle(fontSize: 26, color: Colors.white),
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: Container(
+                color: Colors.white,
+                child: ListView(
+                  children: [
+                    ListTile(
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Profile(title: "Profile"),
+                        ),
+                      ),
+                      leading: Icon(
+                        Icons.account_circle,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      title: Text(
+                        'View Profile',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                    ),
+                    Divider(),
+                    ListTile(
+                      onTap: () {
+                        isDark = !isDark;
+                        isDark
+                            ? ThemeController.of(context)
+                                .setTheme("dark", isDark)
+                            : ThemeController.of(context)
+                                .setTheme("light", isDark);
+                      },
+                      leading: Icon(
+                        Icons.brightness_medium,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      title: Text(
+                        'Dark Mode',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: Switch(
+                        value: isDark,
+                        onChanged: (value) {
+                          setState(() {
+                            isDark = value;
+                            isDark
+                                ? ThemeController.of(context)
+                                    .setTheme("dark", isDark)
+                                : ThemeController.of(context)
+                                    .setTheme("light", isDark);
+                          });
+                        },
+                        activeColor: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                    Divider(),
+                    ListTile(
+                      onTap: () {},
+                      leading: Icon(
+                        Icons.logout,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      title: Text(
+                        'Sign Out',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: Icon(Icons.arrow_forward_ios),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                isDark = !isDark;
-                isDark
-                    ? ThemeController.of(context).setTheme("dark", isDark)
-                    : ThemeController.of(context).setTheme("light", isDark);
-              },
-              child: const Text('Switch Theme',
-                  style: TextStyle(fontSize: 26, color: Colors.white)),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Sign Out',
-                  style: TextStyle(fontSize: 26, color: Colors.white)),
             ),
           ],
         ),
