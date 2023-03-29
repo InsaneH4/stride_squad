519663bdf972ae99b35694a247f26ea746167ef1
Sam H <samuelhussain@live.com>
2023-03-29 18:23:52 -0400

    made cards rounded

--- lib/screens/leaderboard.dart
+++ lib/screens/leaderboard.dart
@@ -15,33 +15,29 @@ class _LeaderboardState extends State<Leaderboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      Container(
+        padding: const EdgeInsets.only(
+            left: 40.0, top: 30.0, right: 40.0, bottom: 4.0),
+        //child: Text("Leaderboard",
+        //  style: Theme.of(context).textTheme.headlineSmall),
+      ),
+      // Horizontal Row containing Top 3 Teams
+      Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Container(
-              padding: const EdgeInsets.only(left: 40.0, top: 30.0, right: 40.0, bottom: 4.0),
-              //child: Text("Leaderboard",
-                //  style: Theme.of(context).textTheme.headlineSmall),
+          Column(children: [
+            const CircleAvatar(
+              backgroundImage:
+                  AssetImage("image_assets/Stock_LeaderBoard_image.png"),
+              radius: 35,
             ),
-          // Horizontal Row containing Top 3 Teams
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [ 
-              Column(
-                      children: [
-                        const CircleAvatar(
-                          backgroundImage: AssetImage(
-                              "image_assets/Stock_LeaderBoard_image.png"),
-                          radius: 35,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0, bottom: 0.1),
-                          child: Text(
-                            "Team 1",
-                            style: Theme.of(context).textTheme.titleSmall
-                              )
-                          ),
-                        /*Padding(
+            Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 0.1),
+                child: Text("Team 1",
+                    style: Theme.of(context).textTheme.titleSmall)),
+            /*Padding(
                           padding: const EdgeInsets.only(top: 5.0, bottom: 0.1),
                           child: Text(
                             "1",
@@ -49,23 +45,20 @@ class _LeaderboardState extends State<Leaderboard> {
                               )
                           ),  
                           */
-                      ]
-              ),
-              Column(
-                      children: [
-                        const CircleAvatar(
-                          backgroundImage: AssetImage(
-                              "image_assets/Stock_LeaderBoard_image.png"),
-                          radius: 35,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8.0, bottom: 0.1),
-                          child: Text(
-                            "Team 2",
-                            style: Theme.of(context).textTheme.titleSmall,
-                              )
-                          ),
-                          /*
+          ]),
+          Column(children: [
+            const CircleAvatar(
+              backgroundImage:
+                  AssetImage("image_assets/Stock_LeaderBoard_image.png"),
+              radius: 35,
+            ),
+            Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 0.1),
+                child: Text(
+                  "Team 2",
+                  style: Theme.of(context).textTheme.titleSmall,
+                )),
+            /*
                           Padding(
                           padding: const EdgeInsets.only(top: 5.0, bottom: 0.1),
                           child: Text(
@@ -74,79 +67,76 @@ class _LeaderboardState extends State<Leaderboard> {
                               )
                           ),
                           */
-                      ]
-              ),
-              Column(
-                      children: [
-                        const CircleAvatar(
-                          backgroundImage: AssetImage(
-                              "image_assets/Stock_LeaderBoard_image.png"),
-                          radius: 35,
-                        ),
-                      Padding(
-                          padding: const EdgeInsets.only(top: 8.0, bottom: 0.1),
-                            child: Text(
-                              "Team 3",
-                              style: Theme.of(context).textTheme.titleSmall,
-                                )
-                            ),
-                            /*Padding(
+          ]),
+          Column(children: [
+            const CircleAvatar(
+              backgroundImage:
+                  AssetImage("image_assets/Stock_LeaderBoard_image.png"),
+              radius: 35,
+            ),
+            Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 0.1),
+                child: Text(
+                  "Team 3",
+                  style: Theme.of(context).textTheme.titleSmall,
+                )),
+            /*Padding(
                             padding: const EdgeInsets.only(top: 5.0, bottom: 0.1),
                             child: Text(
                               "3",
                               style: Theme.of(context).textTheme.titleMedium,
                                 )
                            ), 
-                           */  
-                      ]
+                           */
+          ]),
+        ],
+      ),
+      const Divider(
+        height: 2,
+      ),
+      Expanded(
+        child: ListView.builder(
+          padding: const EdgeInsets.only(top: 10),
+          itemCount: 17,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) => SizedBox(
+            height: 90,
+            child: Card(
+                child: ListTile(
+              tileColor: Colors.white.withAlpha(0),
+              //shape:Border.all(width: 0.5),
+              trailing: Transform(
+                transform: Matrix4.translationValues(-90, 0.0, 0.0),
+                child: const Text(
+                  'RedDashers',
+                  style: TextStyle(fontSize: 32, color: Colors.black),
+                ),
               ),
-          ],),
-          const Divider(
-            height: 2,
+              leading: Transform(
+                transform: Matrix4.translationValues(2, 0.0, 0.0),
+                child: Text(
+                  '${index + 4}',
+                  style: const TextStyle(fontSize: 26, color: Colors.black),
+                ),
+              ),
+              // onTap: showTeamDialog,
+              title: Transform(
+                transform: Matrix4.translationValues(-50, 0.0, 0.0),
+                child: const CircleAvatar(
+                  radius: 22,
+                  backgroundImage:
+                      AssetImage("image_assets/Stock_LeaderBoard_image.png"),
+                ),
+              ),
+            )),
           ),
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.only(top: 10),
-              itemCount: 17,
-              scrollDirection: Axis.vertical,
-              itemBuilder: (context, index) => Card(
-                  child: ListTile(
-                    tileColor: Colors.white,
-                    //shape:Border.all(width: 0.5),
-                    trailing: Transform(
-                      transform: Matrix4.translationValues(-90, 0.0, 0.0), 
-                        child: const Text(
-                          'RedDashers',
-                          style: TextStyle(fontSize: 32, color: Colors.black), 
-                        ),
-                        ),
-                    leading: Transform(
-                      transform: Matrix4.translationValues(2, 0.0, 0.0),
-                        child: Text(
-                          '${index + 4}',
-                          style: const TextStyle(fontSize: 26, color: Colors.black),
-                        ),
-                    ),
-                    // onTap: showTeamDialog,
-                    title: Transform(
-                      transform: Matrix4.translationValues(-50, 0.0, 0.0), 
-                         child: const CircleAvatar(
-                          radius: 22,
-                          backgroundImage: AssetImage(
-                              "image_assets/Stock_LeaderBoard_image.png"),
-                ),
-                ),
-          )
-              ),
-            )
-                ),
-              ]
-        )
-      ); //,
+        ),
+      ),
+    ])); //,
   }
-} 
+}
 
-void alertDialog(BuildContext context){
+void alertDialog(BuildContext context) {
   var alert = const AlertDialog(
     title: Text("My title"),
     content: Text("Dialog description"),
