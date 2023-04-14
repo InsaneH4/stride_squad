b58b05a1264a4835e89e46fd41711b63fbdd41b7
Sam H <samuelhussain@live.com>
2023-04-14 00:25:39 -0400

    update profile page layout

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import '/helpers/auth_check.dart';
 import '/helpers/auth_service.dart';
 import '/main.dart';
 
@@ -55,208 +54,86 @@ class _ProfileState extends State<Profile> {
           ),
         ],
       ),
-      // body: Column(
-      //   crossAxisAlignment: CrossAxisAlignment.start,
-      //   children: [
-      //     Padding(
-      //       padding: const EdgeInsets.only(left: 18.0),
-      //       child: Container(
-      //         decoration: BoxDecoration(
-      //           color: Colors.teal,
-      //           borderRadius: BorderRadius.circular(10.0),
-      //         ),
-      //         child: const Padding(
-      //           padding: EdgeInsets.all(8.0),
-      //           child: Text(
-      //             'First Name: ',
-      //             style: TextStyle(
-      //               color: Colors.white,
-      //               fontSize: 24.0,
-      //               fontWeight: FontWeight.bold,
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      //     ),
-      //     const SizedBox(height: 5.0),
-      //     Padding(
-      //       padding: const EdgeInsets.only(left: 18.0),
-      //       child: Container(
-      //         decoration: BoxDecoration(
-      //           color: Colors.teal,
-      //           borderRadius: BorderRadius.circular(10.0),
-      //         ),
-      //         child: const Padding(
-      //           padding: EdgeInsets.all(8.0),
-      //           child: Text(
-      //             'Last Name: ',
-      //             style: TextStyle(
-      //               color: Colors.white,
-      //               fontSize: 24.0,
-      //               fontWeight: FontWeight.bold,
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      //     ),
-      //     const SizedBox(height: 5.0),
-      //     Padding(
-      //       padding: const EdgeInsets.only(left: 18.0),
-      //       child: Container(
-      //         decoration: BoxDecoration(
-      //           color: Colors.teal,
-      //           borderRadius: BorderRadius.circular(10.0),
-      //         ),
-      //         child: const Padding(
-      //           padding: EdgeInsets.all(8.0),
-      //           child: Text(
-      //             'Date Joined: ',
-      //             style: TextStyle(
-      //               color: Colors.white,
-      //               fontSize: 24.0,
-      //               fontWeight: FontWeight.bold,
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      //     ),
-      //     const SizedBox(height: 5.0),
-      //     Padding(
-      //       padding: const EdgeInsets.only(left: 18.0),
-      //       child: Container(
-      //         decoration: BoxDecoration(
-      //           color: Colors.teal,
-      //           borderRadius: BorderRadius.circular(10.0),
-      //         ),
-      //         child: const Padding(
-      //           padding: EdgeInsets.all(8.0),
-      //           child: Text(
-      //             'Lifetime Steps: ',
-      //             style: TextStyle(
-      //               color: Colors.white,
-      //               fontSize: 24.0,
-      //               fontWeight: FontWeight.bold,
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      //     ),
-      //     const SizedBox(height: 5.0),
-      //     Padding(
-      //       padding: const EdgeInsets.only(left: 18.0),
-      //       child: Container(
-      //         decoration: BoxDecoration(
-      //           color: Colors.teal,
-      //           borderRadius: BorderRadius.circular(10.0),
-      //         ),
-      //         child: const Padding(
-      //           padding: EdgeInsets.all(8.0),
-      //           child: Text(
-      //             'Weekly Average Steps: ',
-      //             style: TextStyle(
-      //               color: Colors.white,
-      //               fontSize: 24.0,
-      //               fontWeight: FontWeight.bold,
-      //             ),
-      //           ),
-      //         ),
-      //       ),
-      //     ),
-      //     /*Row(
-      //       children: [
-      //         Padding(
-      //           padding: const EdgeInsets.only(top: 18.0, left: 18.0),
-      //           child: Text(
-      //             "Steps goal: ",
-      //             style: Theme.of(context).textTheme.titleLarge,
-      //           ),
-      //         ),
-      //         Padding(
-      //           padding: const EdgeInsets.only(top: 18.0, left: 18.0),
-      //           child: TextField(
-      //             controller: goalController,
-      //             decoration: const InputDecoration(
-      //               border: UnderlineInputBorder(),
-      //               labelText: 'Steps',
-      //             ),
-      //             onEditingComplete: () {
-      //               setState(() {
-      //                 stepsGoal = goalController.text;
-      //               });
-      //             },
-      //           ),
-      //         ),
-      //       ],
-      //     ),*/
-      //   ],
-      // ),
-      body: Center(
-        child: Column(
-          //  mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              width: 150,
-              height: 150,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('assets/default_pfp.jpg'),
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-            Text(
-              '@StepsGod69',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            Text(
-              'Member since ${DateFormat('M/d/yy').format(DateTime.now())}',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Text(
-              'Total steps: 2,654,327',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: TextField(
-                  style: const TextStyle(color: Colors.black),
-                  controller: goalController,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.teal),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/default_pfp.jpg'),
+                      fit: BoxFit.fill,
                     ),
-                    labelStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    border: OutlineInputBorder(),
-                    labelText: 'Enter Steps Goal',
-                    filled: true,
-                    fillColor: Colors.white,
                   ),
-                  onSubmitted: (value) {
-                    setState(() {
-                      stepsGoal = int.parse(value);
-                    });
-
-                    //     debugPrint('Steps Goal: $stepsGoal');
-                  }),
-            ),
-            TextButton(
-              child: const Text(
-                'Sign Out',
-                style: TextStyle(
-                  color: Colors.teal,
-                  fontSize: 28,
                 ),
               ),
-              onPressed: () {
-                AuthService().signOut();
-              },
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: Text(
+                  '@StepsGod69',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'Member since ${DateFormat('M/d/yy').format(DateTime.now())}',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: Text(
+                  'Total steps: 2,654,327',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              SizedBox(
+                width: 250,
+                height: 75,
+                child: TextField(
+                    style: Theme.of(context).textTheme.titleMedium,
+                    controller: goalController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.teal),
+                      ),
+                      labelStyle: Theme.of(context).textTheme.titleSmall,
+                      border: const OutlineInputBorder(),
+                      labelText: 'Daily Steps Goal',
+                    ),
+                    onSubmitted: (value) {
+                      setState(() {
+                        stepsGoal = int.parse(value);
+                      });
+
+                      //     debugPrint('Steps Goal: $stepsGoal');
+                    }),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: TextButton(
+                  child: const Text(
+                    'Sign Out',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 28,
+                    ),
+                  ),
+                  onPressed: () {
+                    AuthService().signOut();
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
