ab0ac9221579911811bbfe8d4b75ba2b7c2bc7bf
Benjamin Mingst <benjaminmingst@outlook.com>
2023-04-19 18:14:41 -0400

    Co-authored-by: Anju <anjunthomas@users.noreply.github.com> Co-authored-by: Sam H <InsaneH4@users.noreply.github.com>

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,25 +1,50 @@
 import 'package:flutter/material.dart';
 
-class Chat extends StatelessWidget {
+class Chat extends StatefulWidget {
   const Chat({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
+  _ChatState createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  final TextEditingController _controller = TextEditingController();
+
+  List<String> messages = [];
+
+  void _handleSubmit(String text) {
+    _controller.clear();
+    setState(() {
+      messages.add(text);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                'In progress...',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
+      appBar: AppBar(
+        title: Text('Chat'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Text(messages[index]);
+              },
+            ),
           ),
-        ),
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              hintText: 'Enter a message',
+            ),
+            onSubmitted: _handleSubmit,
+          ),
+        ],
       ),
     );
   }
